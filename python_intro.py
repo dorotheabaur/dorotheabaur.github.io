103ff23b2b9676f194c80941358bc045e30cb391
Dorothea Baur <dorology@gmail.com>
2017-10-14 16:54:10 +0200

    fourth commit: just whatever

---
+++ python_intro.py
@@ -0,0 +1,14 @@
+participants = 62
+if participants < 50:
+    print('it is not busy here')
+elif 30 <= participants < 40:
+    print('at least some are here')
+elif 20 <= participants < 30:
+    print ('I expected more than that')
+elif 50 <= participants < 70:
+    print ('I like it')
+# if not enough people are around, be critical
+def hi():
+    print('huuhu')
+    print('alles ok?')
+hi()
